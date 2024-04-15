0aed10cb86be01de2137d0532979efb6b129ba50
Joao Barres <joao1.joao3@gmail.com>
2024-04-14 21:51:33 -0500

    Implementacios de SpeedInsights

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,9 @@ import App from './App.jsx'
 import { I18nextProvider } from 'react-i18next';
 import i18next from 'i18next';
 
+//Metricas
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 //Archivos json para los idiomas
 import global_es from './componentes/translate/es-latam/global.json';
 import global_en from './componentes/translate/en-usa/global.json';
@@ -36,6 +39,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <NextUIProvider>
       <I18nextProvider i18n={i18next}>
         <App />
+        <SpeedInsights />
       </I18nextProvider>
     </NextUIProvider>
   </React.StrictMode>
